48c84b1f07962679ce8883ddc19f88a155ec6cb2
mujapes <222509507+mujapes@users.noreply.github.com>
2025-10-31 00:08:03 +0000

    Create Q0263-Ugly-Number.rs

---
+++ Easy/Q0263-Ugly-Number.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn is_ugly(mut n: i32) -> bool {
+        if n < 1 {return false;}
+        if n == 1 {return true;}
+        while n > 1 {
+            if n % 5 == 0 {
+                n /= 5;
+            } else if n % 3 == 0 {
+                n /= 3;
+            } else if n % 2 == 0 {
+                n /= 2;
+            } else {return false;}
+        }
+        true
+    }
+}
+
+// Runtime: 0 ms, Beats 100.00%
+// Memory: 2.17 MB, Beats 53.66%
