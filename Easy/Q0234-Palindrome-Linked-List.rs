eed26df6002cae1638f0f411fdac983479a1cca1
mujapes <222509507+mujapes@users.noreply.github.com>
2025-11-20 18:22:09 +0000

    Update Q0234-Palindrome-Linked-List.rs

--- Easy/Q0234-Palindrome-Linked-List.rs
+++ Easy/Q0234-Palindrome-Linked-List.rs
@@ -14,32 +14,24 @@
 //     }
 //   }
 // }
+use std::ptr;
+
 impl Solution {
     pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
-        let mut mid_distance = u16::MAX;
         let mut front = &head;
-        fn pal (back: &Option<Box<ListNode>>, front: &mut &Option<Box<ListNode>>, mid_distance: &mut u16) -> bool {
-            if let Some(end) = back {
-                *mid_distance += 1;
-                if pal(&end.next, front, mid_distance) {
-                    if *mid_distance < 0 {return true}
-                    *mid_distance -= 1;
-                    if let Some(start) = *front {
-                        if start.val == end.val {
-                            *front =  &start.next;
-                            return true
-                        }
-                    }
-                }
-                false
-            } else {
-                *mid_distance /= 2;
-                true
-            }
+        let mut done = false;
+        fn pal (back: &Option<Box<ListNode>>, front: &mut &Option<Box<ListNode>>, done: &mut bool) -> bool {
+            back.as_ref().is_none_or(|end| {
+                pal(&end.next, front, done) && (*done || (*front).as_ref().is_some_and(|start| {
+                    if ptr::eq(*front, back) || ptr::eq(&start.next, back) {*done = true;}
+                    *front = &start.next;
+                    start.val == end.val
+                }))
+            })
         }
-        pal(&head, &mut front, &mut mid_distance)
+        pal(&head, &mut front, &mut done)
     }
 }
 
-// Runtime: 20 ms, Beats 37.23%
-// Memory: 9.16 MB. Beats 44.68%
+// Runtime: 7 ms, Beats 92.55%
+// Memory: 12.42 MB, Beats 6.38%
