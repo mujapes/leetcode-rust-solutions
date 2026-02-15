9af9804eb6c61327489f2d5a4099634c28f3d8af
mujapes <222509507+mujapes@users.noreply.github.com>
2026-02-15 13:18:50 +0000

    Update Q0509-Fibonacci-Number.rs

--- Easy/Q0509-Fibonacci-Number.rs
+++ Easy/Q0509-Fibonacci-Number.rs
@@ -1,9 +1,11 @@
 impl Solution {
     pub fn fib(n: i32) -> i32 {
         if n < 2 {return n}
-        Self::fib(n-1) + Self::fib(n-2)
+        (2..=n)
+            .fold( (0, 1), |(first, second), _| (second, first + second) )
+            .1
     }
 }
 
-// Runtime: 7 ms, Beats 7.43%
-// Memory: 2.14 MB, Beats 39.11%
+// Runtime: 0 ms, Beats 100.00%
+// Memory: 2.16 MB, Beats 39.11%
