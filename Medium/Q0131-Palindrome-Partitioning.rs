8c594a19ec99bc839c7e2cfe70f6aa360655bd76
mujapes <222509507+mujapes@users.noreply.github.com>
2026-05-03 15:34:02 +0100

    Update Q0131-Palindrome-Partitioning.rs

--- Medium/Q0131-Palindrome-Partitioning.rs
+++ Medium/Q0131-Palindrome-Partitioning.rs
@@ -17,31 +17,34 @@ impl Solution {
             trivial.push(c.to_string());
         }
         
-        fn recurse(part: Vec<String>, parts: &mut HashSet<Vec<String>>) {
+        fn recurse(part: Vec<String>, parts: &mut HashSet<Vec<String>>, unpartitioned_start: usize) {
             parts.insert(part.clone());
-            for i in 0..part.len()-1 {
+            for i in unpartitioned_start..part.len()-1 {
                 // 2 element palindrom builder
                 if part[i].len() == 1 && part[i+1] == part[i] {
                     let mut new_part = Vec::with_capacity(part.len()-1);
                     new_part.extend_from_slice(&part[0..i]);
                     new_part.push( format!("{}{}", part[i], part[i]) );
                     new_part.extend_from_slice(&part[i+2..part.len()]);
-                    recurse(new_part, parts);
+                    recurse(new_part, parts, i);
                 }
                 if i > 0 {
                     // palindrome expansion
                     if part[i-1] == part[i+1].chars().rev().collect::<String>() {
-                        let mut new_part = Vec::with_capacity(part.len()-1);
+                        let mut new_part = Vec::with_capacity(part.len()-2);
                         new_part.extend_from_slice(&part[0..i-1]);
                         new_part.push( format!("{}{}{}", part[i-1], part[i], part[i+1]) );
                         new_part.extend_from_slice(&part[i+2..part.len()]);
-                        recurse(new_part, parts);
+                        recurse(new_part, parts, i-1);
                     }
                 }
             }
         }
 
-        recurse(trivial, &mut parts);
+        recurse(trivial, &mut parts, 0);
         parts.into_iter().collect()
     }
 }
+
+// Runtime: 65 ms, Beats 6.25%
+// Memory: 20.89 MB, Beats 4.17%
