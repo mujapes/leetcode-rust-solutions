13b719ac2dc5b28da4703f4b60fe6f5bfbfaa948
mujapes <222509507+mujapes@users.noreply.github.com>
2026-05-14 19:35:12 +0100

    Create Q0153-Find-Minimum-in-Rotated-Sorted-Array.rs

---
+++ Medium/Q0153-Find-Minimum-in-Rotated-Sorted-Array.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left < right {
+            let mid = left + (right-left)/2;
+            if nums[mid] < nums[right] {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        nums[left]
+    }
+}
+
+// Runtime: 0 ms, Beats 100.00%
+// Memory: 2.26 MB, Beats 18.60%
